aa77df1fe0c26311289a2d9b8721d38a27a5d4d7
StonePunch <antonio.lopes1997@gmail.com>
2024-01-09 23:03:32 +0000

    feat: added errorJSON helper

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -24,6 +24,8 @@ func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) er
 		return err
 	}
 
+	// The decoded value here does not matter, so &struct{}{} is used
+	// this logic is just to check that the input only has one JSON value
 	err = decoder.Decode(&struct{}{})
 	if err != io.EOF {
 		return errors.New("body must have only a single JSON value")
@@ -32,14 +34,14 @@ func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) er
 	return nil
 }
 
-func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header *http.Header) error {
 	out, err := json.Marshal(data)
 	if err != nil {
 		return nil
 	}
 
-	if len(headers) != 0 {
-		for key, value := range headers[0] {
+	if header != nil {
+		for key, value := range *header {
 			w.Header()[key] = value
 		}
 	}
@@ -49,4 +51,17 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 	_, err = w.Write(out)
 
 	return err
+}
+
+func (app *Config) errorJSON(w http.ResponseWriter, err error, status *int) error {
+	statusCode := http.StatusBadRequest
+	if status != nil {
+		statusCode = *status
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJSON(w, statusCode, payload, nil)
 }
