3ea47601297f4432f46aaf7652493d70bd8772e0
StonePunch <antonio.lopes1997@gmail.com>
2024-01-10 20:45:36 +0000

    feat(auth): added bare-bones server config

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"authentication/data"
 	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
 )
 
 const port = 80
@@ -15,4 +17,19 @@ type Config struct {
 
 func main() {
 	log.Println("Starting authentication service")
+
+	// TODO: connect to db
+
+	// set up config
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }
--- authentication-service/go.mod
+++ authentication-service/go.mod
@@ -2,4 +2,8 @@ module authentication
 
 go 1.21.3
 
-require golang.org/x/crypto v0.18.0
+require (
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/go-chi/cors v1.2.1
+	golang.org/x/crypto v0.18.0
+)
