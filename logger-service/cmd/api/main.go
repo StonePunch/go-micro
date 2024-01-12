a0561a115ccfe83fb866548fef868871141ec620
StonePunch <antonio.lopes1997@gmail.com>
2024-01-12 23:16:44 +0000

    feat(logger): added func to connect to mongo

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
@@ -15,4 +19,22 @@ type Config struct {
 }
 
 func main() {
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect to mongo
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting:", err)
+		return nil, err
+	}
+
+	return c, nil
 }
