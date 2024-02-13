c1b686fe5cb85b31160ef48694561e53e36e28ec
StonePunch <antonio.lopes1997@gmail.com>
2024-02-13 20:02:54 +0400

    feat(mail): added initial boilerplate for the mail-service

---
+++ mail-service/cmd/api/helpers.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := 1048576 // 1 MB
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	// The decoded value here does not matter, so &struct{}{} is used
+	// this logic is just to check that the input only has one JSON value
+	err = decoder.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must have only a single JSON value")
+	}
+
+	return nil
+}
+
+func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return nil
+	}
+
+	if len(header) != 0 {
+		for key, value := range header[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+
+	return err
+}
+
+func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+	if len(status) != 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJSON(w, statusCode, payload, nil)
+}
