6ebdb5d8a9435fe012bed1f37f62b8296fc69a67
StonePunch <antonio.lopes1997@gmail.com>
2024-02-13 22:34:27 +0400

    feat(mail): added Mail type initialization

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 const webPort = 80
 
 type Config struct {
+	Mailer Mail
 }
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	log.Println("Starting mail service on port", webPort)
 
@@ -25,4 +30,25 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func createMail() Mail {
+	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	if err != nil {
+		log.Panic(err)
+	}
+	encryption := Encryption(os.Getenv("MAIL_ENCRYPTION"))
+
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  encryption,
+		FromName:    os.Getenv("MAIL_FROM_NAME"),
+		FromAddress: os.Getenv("MAIL_FROM_ADDRESS"),
+	}
+
+	return m
 }
